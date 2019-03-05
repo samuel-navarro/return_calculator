eb16abfebe06edec9b0387e6233ba0226561b062
Samuel Navarro Lou <samuel.navarrolou@trimble.com>
2019-03-05 15:31:56 +0200

    Add configuration options
    The following has been added:
        - Selection of the montly investment: If present, the final worth
          will be printed, otherwise only the return will be printed
    
        - Selection of start and end years: To calculate returns within
          limited dates only
    
        - Option to draw history: The history of the index is now optionally
          drawn, to prevent stopping the program for no good reason.

--- index_process.py
+++ index_process.py
@@ -1,32 +1,27 @@
 import matplotlib.pyplot as pl
 import numpy as np
 import csv
-import itertools as it
+import sys
 import argparse
+import scipy.optimize as opt
 
-def get_month(date_str):
+
+def get_year(date_str):
     split_date = date_str.split('-')
-    return int(split_date[1])
+    return int(split_date[0])
 
 
-def read_index_file(csv_file_name):
+def read_index_file(csv_file_name, start_year, end_year):
     with open(csv_file_name) as f:
         csv_reader = csv.DictReader(f)
-        first_date = next(csv_reader.__iter__())
 
         index_data = {key: [] for key in csv_reader.fieldnames}
         for row in csv_reader:
+            year = get_year(row['Date'])
+            if (year < start_year) or (year > end_year):
+                continue
             for key, value in row.items():
                 index_data[key].append(value)
-
-        months = list(map(get_month, index_data['Date']))
-        repeated_indices = list()
-        for i, month in enumerate(months):
-            if months[i] == months[i-1]:
-                repeated_indices.append(i)
-
-        for key in index_data.keys():
-            index_data[key] = [x for i, x in enumerate(index_data[key]) if i not in repeated_indices]
 
     return index_data
 
@@ -50,25 +45,82 @@ def calc_return(monthly_highs, amount_per_time, month_freq):
     return final_worth / total_invested, total_invested
 
 
-if __name__ == '__main__':
+def get_average_yearly_return(investment_return, number_of_months):
+    n_years = number_of_months / 12
+
+    def target_fun(interest):
+        return_at_interest = ((1 + interest)**n_years - 1) / ((1+interest)**(1/12) - 1)
+        return_respect_to_total_invested = return_at_interest / number_of_months
+
+        return return_respect_to_total_invested - investment_return
+    
+    INITIAL_GUESS = 0.07
+    return opt.newton(target_fun, INITIAL_GUESS)
+
+
+def set_up_arguments():
     parser = argparse.ArgumentParser(description='Process monthly historical data from an index')
-    parser.add_argument('--index_file',
+    parser.add_argument('-f', '--index_file',
                         default='index.csv',
                         help='File where the desired index data resides (csv)')
+    parser.add_argument('-d', '--draw_history',
+                        action='store_true',
+                        help='If present, the history of the index is drawn before printing the statistics')
+    parser.add_argument('-m', '--monthly_investment',
+                        default=None,
+                        type=float,
+                        help='Monthly investment, to calculate the final worth')
+    parser.add_argument('-y0', '--start_year',
+                        default=0,
+                        type=int,
+                        help='Year where investment starts')
+    parser.add_argument('-y1', '--end_year',
+                        default=sys.maxsize,
+                        type=int,
+                        help='Year where investment ends')
+    
+    return parser
 
+
+if __name__ == '__main__':
+    parser = set_up_arguments()
     args = parser.parse_args()
-    index_data = read_index_file(args.index_file)
+    
+    index_data = read_index_file(args.index_file, args.start_year, args.end_year)
 
     monthly_highs = list(map(float, index_data['High']))
 
-    month_investment_return, total_invested = calc_return(monthly_highs, 1000, 1)
-    year_investment_return, total_invested = calc_return(monthly_highs, 12000, 12)
+    monthly_investment = 1000
+    print_worth = False
+    if args.monthly_investment is not None:
+        monthly_investment = args.monthly_investment
+        print_worth = True
 
-    print('Return with monthly investment: {}. Total worth: {}'
-          .format(month_investment_return, month_investment_return * total_invested))
-    print('Return with single yearly investment: {}. Total worth: {}'
-          .format(year_investment_return, year_investment_return * total_invested))
+    month_investment_return, total_invested = calc_return(monthly_highs, monthly_investment, 1)
+    year_investment_return, total_invested = calc_return(monthly_highs, 12 * monthly_investment, 12)
+
+    n_months_invested = len(monthly_highs)
+
+    print('Return multiplier with monthly investment: {:.2f}'
+          .format(month_investment_return))
+    print('Average return per year: {:.2f}%'
+          .format(100 * get_average_yearly_return(month_investment_return, n_months_invested)))
+    if (print_worth):
+        print('Total invested: {:,.2f}. Total worth: {:,.2f}'.format(total_invested, month_investment_return * total_invested))
+    
+    print()
+
+    print('Return with single yearly investment: {:.2f}'
+          .format(year_investment_return))
+    print('Average return per year: {:.2f}%'
+          .format(100 * get_average_yearly_return(year_investment_return, n_months_invested)))
+    if (print_worth):
+        print('Total invested: {:,.2f}. Total worth: {:,.2f}'.format(total_invested, year_investment_return * total_invested))
+
+    print()
+
     print('Invested from {} until {}'
           .format(index_data['Date'][0], index_data['Date'][-1]))
 
-    plot_graph(index_data)
+    if args.draw_history:
+        plot_graph(index_data)
